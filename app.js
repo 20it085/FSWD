f0c902216976cc7fa41c1734db80f69b924bc4d9
20it085 <88103864+20it085@users.noreply.github.com>
2022-09-27 13:03:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const{add, sub, mul, div} = require('./index.js');
+const argvs = process.argv
+const argv = argvs.slice(2)
+const operation = argv[0]
+const operator1 = parseInt(argv[1])
+const operator2 = parseInt(argv[2]) 
 
-console.log(add(3,4));
 
-console.log(sub(3,4));
-
-console.log(mul(3,4));
-
-console.log(div(3,4));  
+if (operation === 'add') {
+	console.log(operation + ' is '
+		+ (operator1 + operator2));
+}
+if (operation === 'subtract') {
+	console.log(operation + ' is '
+		+ (operator1 - operator2));
+}
+if (operation === 'multiply') {
+	console.log(operation + ' is '
+		+ (operator1 + operator2));
+}
+if (operation === 'divide') {
+ 	console.log(operation + ' is '
+		+ (operator1 - operator2));
+}
